198a438991c47999d850dd6694494710764490ec
Ubuntu <ubuntu@ip-172-31-54-217.ec2.internal>
2020-10-11 04:10:20 +0000

    add user login api

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,65 @@
+class UsersController < ApplicationController
+  
+  before_action :authenticate_request!, except: :create
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  # GET /users
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+  
+  def login
+    user = User.find_by(email: user_params[:email].to_s.downcase)
+
+    if user&.authenticate(user_params[:password])
+      auth_token = JsonWebToken.encode(user_id: user.id)
+      render json: { auth_token: auth_token }, status: :ok
+    else
+      render json: { error: 'Invalid username/password' }, status: :unauthorized
+    end
+  end
+  
+
+  # GET /users/1
+  def show
+    render json: @user
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  resources :users 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace 'api' do
       namespace 'v1' do
           resources :campaigns
       end
   end
+  resources :users do
+    collection do
+      post 'login'
+    end
+  end
+  
 end
