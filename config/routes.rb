8c9f3cbf7cf1da1b789b21732a496af1f173e81e
Ubuntu <ubuntu@ip-172-31-54-217.ec2.internal>
2020-10-11 04:16:43 +0000

    adding routes to api instead of directly

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
   namespace 'api' do
       namespace 'v1' do
           resources :campaigns
-      end
-  end
-  resources :users do
+          resources :users do
     collection do
       post 'login'
     end
   end
+      end
+  end
+  
   
 end
